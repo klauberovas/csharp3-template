d96a80abdad227443c971fe225548c363d9565a4
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2025-10-02 09:52:45 +0200

    kód z lekce 02

---
+++ ToDoList/src/ToDoList.WebApi/Program.cs
@@ -0,0 +1,9 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/test", () => "Test");
+app.MapGet("/pozdrav/{jmeno}", (string jmeno) => $"Ahoj {jmeno}");
+app.MapGet("/secti/{a:int}/{b:int}", (int a, int b) => $"Výsledek {a} + {b} = {a + b}");
+
+app.Run();
